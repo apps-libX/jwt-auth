92d9bda81e3d5d87551ca44e7364ccbc80136555
anonymoussc <50c5ac69@opayq.com>
2015-12-09 04:01:26 +0700

    Add new module

---
+++ angular/jwt-auth.module.js
@@ -0,0 +1,16 @@
+/**
+ * Created by anonymous on 08/12/15 16:58.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth', [
+            /* Shared modules */
+            'app.core',
+            'app.widgets'
+            /*----------------*/
+        ]);
+
+})();
