a6de69784929d9e34d5743a22019d0b4dd15ffcd
anonymoussc <50c5ac69@opayq.com>
2015-12-08 16:57:14 +0700

    Add login

---
+++ angular/applications/jwt-auth/login/login.controller.js
@@ -0,0 +1,29 @@
+/**
+ * Created by anonymous on 08/12/15 16:23.
+ */
+
+(function() {
+    'use strict';
+
+    angular
+        .module('components.controllers')
+        .controller('LoginController', LoginController);
+
+    LoginController.$inject = [''];
+
+    /* @ngInject */
+    function LoginController() {
+        var vm   = this;
+        vm.title = 'LoginController';
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            //
+        }
+    }
+
+})();
+
