c4fce5abd11da0128fe7c68fa5d0904a990fa947
anonymoussc <50c5ac69@opayq.com>
2015-12-08 11:32:03 +0700

    Update routes.js

--- angular/routes.js
+++ angular/routes.js
@@ -1,13 +1,13 @@
 /**
- * Created by anonymoussc on 01/12/15 23:48.
+ * Created by anonymoussc on 08/12/15 11:20.
  */
 
 (function() {
     'use strict';
 
-    angular.module('components.routes.jwtAuth').config(function($stateProvider, $urlRouterProvider, $httpProvider, $provide) {
-        $provide.factory('redirectWhenLoggedOut', redirectWhenLoggedOut);
-        $httpProvider.interceptors.push('redirectWhenLoggedOut');
+    angular.module('components.routes.jwtAuth').config(function($stateProvider, $urlRouterProvider, $authProvider, $httpProvider, $provide) {
+        // $provide.factory('redirectWhenLoggedOut', redirectWhenLoggedOut);
+        // $httpProvider.interceptors.push('redirectWhenLoggedOut');
         $urlRouterProvider.otherwise('/');
 
         $stateProvider
@@ -66,6 +66,74 @@
                 }
             });
 
+        $authProvider.facebook({
+            clientId: '657854390977827'
+        });
+
+        $authProvider.google({
+            clientId: '631036554609-v5hm2amv4pvico3asfi97f54sc51ji4o.apps.googleusercontent.com'
+        });
+
+        $authProvider.github({
+            clientId: '0ba2600b1dbdb756688b'
+        });
+
+        $authProvider.linkedin({
+            clientId: '77cw786yignpzj'
+        });
+
+        $authProvider.instagram({
+            clientId: '799d1f8ea0e44ac8b70e7f18fcacedd1'
+        });
+
+        $authProvider.yahoo({
+            clientId: 'dj0yJmk9SDVkM2RhNWJSc2ZBJmQ9WVdrOWIzVlFRMWxzTXpZbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0yYw--'
+        });
+
+        $authProvider.twitter({
+            url: '/auth/twitter'
+        });
+
+        $authProvider.live({
+            clientId: '000000004C12E68D'
+        });
+
+        $authProvider.twitch({
+            clientId: 'qhc3lft06xipnmndydcr3wau939a20z'
+        });
+
+        $authProvider.bitbucket({
+            clientId: '48UepjQDYaZFuMWaDj'
+        });
+
+        $authProvider.oauth2({
+            name                 : 'foursquare',
+            url                  : '/auth/foursquare',
+            clientId             : 'MTCEJ3NGW2PNNB31WOSBFDSAD4MTHYVAZ1UKIULXZ2CVFC2K',
+            redirectUri          : window.location.origin || window.location.protocol + '//' + window.location.host,
+            authorizationEndpoint: 'https://foursquare.com/oauth2/authenticate'
+        });
+
+        function skipIfLoggedIn($q, $auth) {
+            var deferred = $q.defer();
+            if ($auth.isAuthenticated()) {
+                deferred.reject();
+            } else {
+                deferred.resolve();
+            }
+            return deferred.promise;
+        }
+
+        function loginRequired($q, $location, $auth) {
+            var deferred = $q.defer();
+            if ($auth.isAuthenticated()) {
+                deferred.resolve();
+            } else {
+                $location.path('/login');
+            }
+            return deferred.promise;
+        }
+
         function view(viewName) {
             if (viewName !== "") {
                 return './views/applications/' + appName(viewName) + '/' + fileDir(viewName) + '/' + fileName(viewName) + '.html';
@@ -113,26 +181,29 @@
             }
         }
 
-        function redirectWhenLoggedOut($q, $injector) {
+        /*
+         function redirectWhenLoggedOut($q, $injector) {
 
-            return {
+         return {
 
-                responseError: function(rejection) {
-                    var
-                        $state           = $injector.get('$state'),
-                        rejectionReasons = ['token_not_provided', 'token_expired', 'token_absent', 'token_invalid'];
+         responseError: function(rejection) {
+         var
+         $state           = $injector.get('$state'),
+         rejectionReasons = ['token_not_provided', 'token_expired', 'token_absent', 'token_invalid'];
 
-                    angular.forEach(rejectionReasons, function(value, key) {
+         angular.forEach(rejectionReasons, function(value, key) {
 
-                        if (rejection.data.error === value) {
-                            localStorage.removeItem('user');
-                            $state.go('jwtauth.signin');
-                        }
-                    });
+         if (rejection.data.error === value) {
+         localStorage.removeItem('user');
+         $state.go('jwtauth.signin');
+         }
+         });
 
-                    return $q.reject(rejection);
-                }
-            };
-        }
+         return $q.reject(rejection);
+         }
+         };
+         }
+         */
+
     });
 })();
