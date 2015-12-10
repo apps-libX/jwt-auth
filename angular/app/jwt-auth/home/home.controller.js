066241cdee3b903c1f325222eb371264d8591730
anonymoussc <50c5ac69@opayq.com>
2015-12-10 20:21:55 +0700

    Update home

--- angular/app/jwt-auth/home/home.controller.js
+++ angular/app/jwt-auth/home/home.controller.js
@@ -2,7 +2,7 @@
  * Created by anonymous on 10/12/15 10:06.
  */
 
-(function() {
+/*(function() {
     'use strict';
 
     angular
@@ -11,7 +11,7 @@
 
     JwtAuthHomeController.$inject = [];
 
-    /* @ngInject */
+    /!* @ngInject *!/
     function JwtAuthHomeController() {
         var vm   = this;
         vm.title = 'JwtAuthHomeController';
@@ -25,5 +25,22 @@
         }
     }
 
-})();
+})();*/
 
+angular.module('jwtAuth')
+    .controller('HomeCtrl', function($scope, $http) {
+        $http.jsonp('https://api.github.com/repos/sahat/satellizer?callback=JSON_CALLBACK')
+            .success(function(data) {
+                if (data) {
+                    if (data.data.stargazers_count) {
+                        $scope.stars = data.data.stargazers_count;
+                    }
+                    if (data.data.forks) {
+                        $scope.forks = data.data.forks;
+                    }
+                    if (data.data.open_issues) {
+                        $scope.issues = data.data.open_issues;
+                    }
+                }
+            });
+    });
