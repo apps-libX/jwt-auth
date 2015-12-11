ab0d2cee2b54c2922bd9ff65fa4fd1a7b8f94660
anonymoussc <50c5ac69@opayq.com>
2015-12-11 10:04:49 +0700

    Update logout.controller.js

--- angular/controllers/logout.controller.js
+++ angular/controllers/logout.controller.js
@@ -2,12 +2,35 @@
  * Created by anonymous on 10/12/15 21:31.
  */
 
-angular.module('jwtAuth')
-    .controller('LogoutCtrl', function($location, $auth, toastr) {
-        if (!$auth.isAuthenticated()) { return; }
-        $auth.logout()
-            .then(function() {
-                toastr.info('You have been logged out');
-                $location.path('/');
-            });
-    });
+(function() {
+    'use strict';
+
+    angular
+        .module('jwtAuth')
+        .controller('JwtAuthLogoutController', JwtAuthLogoutController);
+
+    JwtAuthLogoutController.$inject = ['$location', '$auth', 'toastr'];
+
+    /* @ngInject */
+    function JwtAuthLogoutController($location, $auth, toastr) {
+        var vm   = this;
+        vm.title = 'JwtAuthLogoutController';
+
+        activate();
+
+        ////////////////
+
+        function activate() {
+            if (!$auth.isAuthenticated()) {
+                return;
+            }
+            $auth.logout()
+                .then(function() {
+                    toastr.info('You have been logged out');
+                    $location.path('/');
+                });
+        }
+    }
+
+})();
+
