4b2304f8e8b1b14b247676c1b4e71ec2eb65b3cd
anonymoussc <50c5ac69@opayq.com>
2015-12-11 09:00:02 +0700

    update home.controller.js

--- angular/app/jwt-auth/home/home.controller.js
+++ angular/app/jwt-auth/home/home.controller.js
@@ -2,24 +2,6 @@
  * Created by anonymous on 10/12/15 10:06.
  */
 
-angular.module('jwtAuth')
-    .controller('HomeCtrl', function($scope, $http) {
-        $http.jsonp('https://api.github.com/repos/sahat/satellizer?callback=JSON_CALLBACK')
-            .success(function(data) {
-                if (data) {
-                    if (data.data.stargazers_count) {
-                        $scope.stars = data.data.stargazers_count;
-                    }
-                    if (data.data.forks) {
-                        $scope.forks = data.data.forks;
-                    }
-                    if (data.data.open_issues) {
-                        $scope.issues = data.data.open_issues;
-                    }
-                }
-            });
-    });
-
 (function() {
     'use strict';
 
