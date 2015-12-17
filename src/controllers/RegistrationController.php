015325f8c278305ad354920799c5985499f0a85b
anonymoussc <50c5ac69@opayq.com>
2015-12-17 19:53:07 +0700

    Update RegistrationController, User

---
+++ src/controllers/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by anonymous on 17/12/15 16:34.
+ */
+
+namespace AppsLibX\JwtAuth\Controllers;
+
+use Vinkla\Hashids\HashidsManager;
+use Anwendungen\Application\Controller\Controller;
+use AppsLibX\Sentinel\FormRequests\RegisterRequest;
+use AppsLibX\Sentinel\FormRequests\EmailRequest;
+use AppsLibX\Sentinel\FormRequests\ResetPasswordRequest;
+use AppsLibX\Sentinel\Repositories\Group\SentinelGroupRepositoryInterface;
+use AppsLibX\Sentinel\Repositories\User\SentinelUserRepositoryInterface;
+use Sentry;
+use View;
+use Input;
+use Event;
+use Redirect;
+use Session;
+use Config;
+
+
+class RegistrationController extends Controller
+{
+    public function __construct(
+        SentinelUserRepositoryInterface $userRepository,
+        SentinelGroupRepositoryInterface $groupRepository,
+        HashidsManager $hashids
+    ) {
+        $this->userRepository = $userRepository;
+        $this->groupRepository = $groupRepository;
+        $this->hashids = $hashids;
+    }
+
+    public function register(RegisterRequest $request)
+    {
+        // Gather input
+        $data = Input::all();
+
+        // Attempt Registration
+        $result = $this->userRepository->store($data);
+
+        // It worked!  Use config to determine where we should go.
+        return $this->redirectViaResponse('registration_complete', $result);
+    }
+
+}
