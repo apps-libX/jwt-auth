2dd47d923a04aa9f138d136824622f50deb4ed99
anonymoussc <50c5ac69@opayq.com>
2015-12-18 08:35:39 +0700

    Add UserRepository, UserRepositoryInterface

---
+++ src/JwtAuth/Repositories/User/UserRepository.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by anonymous on 18/12/15 8:35.
+ */
+
+namespace AppsLibX\JwtAuth;
+
+
+class UserRepository
+{
+
+}
---
+++ src/JwtAuth/Repositories/User/UserRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by anonymous on 18/12/15 8:34.
+ */
+
+namespace AppsLibX\JwtAuth;
+
+
+interface UserRepositoryInterface
+{
+
+}
