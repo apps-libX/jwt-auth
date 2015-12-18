83750a10b33ceb9ef7377abd639742e4065a1048
anonymoussc <50c5ac69@opayq.com>
2015-12-18 10:50:10 +0700

    Update UserRepository, Add ExceptionResponse

---
+++ src/JwtAuth/DataTransferObjects/ExceptionResponse.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by anonymous on 18/12/15 10:47.
+ */
+
+namespace AppsLibX\JwtAuth\DataTransferObjects;
+
+class ExceptionResponse extends BaseResponse
+{
+    public function __construct($message, array $payload = null)
+    {
+        parent::__construct($message, $payload);
+
+        $this->success = false;
+        $this->error = true;
+    }
+}
--- src/JwtAuth/Repositories/User/UserRepository.php
+++ src/JwtAuth/Repositories/User/UserRepository.php
@@ -3,10 +3,23 @@
  * Created by anonymous on 18/12/15 8:35.
  */
 
-namespace AppsLibX\JwtAuth;
+namespace AppsLibX\JwtAuth\Repositories\User;
 
+/*use Cartalyst\Sentry\Sentry;
+use Cartalyst\Sentry\Users\UserExistsException;
+use Cartalyst\Sentry\Users\UserNotFoundException;
+use Cartalyst\Sentry\Users\UserAlreadyActivatedException;*/
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\UserProvider;
+use Illuminate\Config\Repository;
+use Illuminate\Events\Dispatcher;
+use AppsLibX\JwtAuth\Models\User;
+use AppsLibX\JwtAuth\DataTransferObjects\BaseResponse;
+//use JwtAuth\DataTransferObjects\SuccessResponse;
+//use JwtAuth\DataTransferObjects\FailureResponse;
+//use JwtAuth\DataTransferObjects\ExceptionResponse;
 
-class UserRepository
+class UserRepository implements UserRepositoryInterface, UserProvider
 {
 
 }
