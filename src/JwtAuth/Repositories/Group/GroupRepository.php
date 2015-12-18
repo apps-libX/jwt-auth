4bfb92057cf679f2094a8df7a0638ff20d32cca5
anonymoussc <50c5ac69@opayq.com>
2015-12-18 08:40:12 +0700

    Add GroupRepository, GroupRepositoryInterface

---
+++ src/JwtAuth/Repositories/Group/GroupRepository.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by anonymous on 18/12/15 8:38.
+ */
+
+namespace AppsLibX\JwtAuth;
+
+
+class GroupRepository
+{
+
+}
---
+++ src/JwtAuth/Repositories/Group/GroupRepositoryInterface.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by anonymous on 18/12/15 8:39.
+ */
+
+namespace AppsLibX\JwtAuth;
+
+
+interface GroupRepositoryInterface
+{
+
+}
