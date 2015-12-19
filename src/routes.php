0557bcdb027483f2f9c40ce2afcb01921fa37ab9
anonymoussc <50c5ac69@opayq.com>
2015-12-19 16:21:47 +0700

    Update routes

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@ $api = app('Dingo\Api\Routing\Router');
 
 $api->version('v1', function ($api) {
     $api->post('authenticate', 'AppsLibX\JwtAuth\Controllers\AuthenticateController@authenticate');
-    $api->post('auth/signup', ['as' => 'register.user', 'uses' => 'RegistrationController@register']);
+    $api->post('auth/signup', ['as' => 'register.user', 'uses' => 'AppsLibX\JwtAuth\Controllers\RegistrationController@register']);
 });
 
 // Protected with JWT
