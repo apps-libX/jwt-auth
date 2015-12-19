6d9d1c68adda65727df2d0273aa2d3414b6b721c
anonymoussc <50c5ac69@opayq.com>
2015-12-19 12:34:59 +0700

    Update UserRepositoryInterface

--- src/JwtAuth/Repositories/User/UserRepositoryInterface.php
+++ src/JwtAuth/Repositories/User/UserRepositoryInterface.php
@@ -7,7 +7,7 @@ namespace AppsLibX\JwtAuth\Repositories\User;
 
 use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Collection;
-use AppsLibX\JwtAuth\DataTransferObjects\BaseResponse;
+use Sentinel\DataTransferObjects\BaseResponse;
 use AppsLibX\JwtAuth\Models\User;
 
 interface UserRepositoryInterface
